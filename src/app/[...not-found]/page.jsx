4c0f30a07ed5fa46e20f241ea9ff0ece0947b332
Sanket <tech@qodeinvest.com>
2024-10-23 15:02:14 +0530

    ui update

--- src/app/[...not-found]/page.jsx
+++ src/app/[...not-found]/page.jsx
@@ -11,7 +11,7 @@ const NotFoundPage = () => {
     <div className="min-h-screen flex flex-col items-center justify-center bg-lightBeige text-center p-8">
       <Heading className=" text-brown mb-4">404 - Page Not Found</Heading>
       <Text className="text-body sm:text-body text-gray-600 mb-6">
-        Sorry, the page you're looking for doesn't exist.
+        Sorry, the page you`&apos;`re looking for doesn`&apos;`t exist.
       </Text>
       <Link href="/" passHref>
         <Button className=" text-black bg-brown ">Go Back Home</Button>
